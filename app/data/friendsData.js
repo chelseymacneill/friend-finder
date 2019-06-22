e9a9a2e643928e641c64bd14b747390671dfd223
cmac <chelsey.macneill@gmail.com>
2019-06-21 20:08:22 -0700

    html files built with bootstrap layout

---
+++ app/data/friendsData.js
@@ -0,0 +1,20 @@
+// Data for filled out surveys
+
+let freindsArray = [
+    {
+        "name": "Charles",
+        "photo": "https://media.licdn.com/mpr/mpr.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    }
+];
