ef1697dfcf5d1ead306bcc12fd1c1def4459993f
cmac <chelsey.macneill@gmail.com>
2019-06-23 13:04:31 -0700

    things were done here I can neither confirm nor deny my pride in what occured

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,28 +6,35 @@
 
 let friendsData = require("../data/friendsData")
 
-// A GET route with the url /api/freinds to display the JSON of all possible friends
-// Displays all characters
-app.get("/api/friends", function(req, res) {
-    return res.json(friends);
-});
 
-// A POST route for /api/freinds that will handle incoming survey results and house compatability logic
+// Routing 
 
-// Create a new friend - takes in JSON input
-app.post("/api/friends", function(req, res) { // the friends was just a place holder for right now
-    // req.body hosts is equal to the JSON post sent from the user
-    // This works because of our body parsing middleware 
-    let newFriend = req.body;
+module.exports = function(app) {
+    app.get("/api/friends"), function(req, res) {
+        res.json(userData);
+    }
     
-    // Using a RegEx Pattern to remove spaces from newCharacter
-    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-    newFriend.routeName = newFreind.name.replace(/\s+/g, "").toLowerCase();
+    // A POST route for /api/freinds that will handle incoming survey results and house compatability logic
+    // Create a new friend - takes in JSON input
     
-    console.log(newFriend);
-    
-    friends.push(newFriend);
-    
-    res.json(newFriend);
-});
+    app.post("/api/friends", function(req, res) { // the friends was just a place holder for right now
+        // req.body hosts is equal to the JSON post sent from the user
+        // This works because of our body parsing middleware 
+        
+        // Where are logical will alert you if you have a match 
+        let newFriend = req.body;
+        
+        // Using a RegEx Pattern to remove spaces from newCharacter
+        // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+        newFriend.routeName = newFreind.name.replace(/\s+/g, "").toLowerCase();
+        
+        console.log(newFriend);
+        
+        friends.push(newFriend);
+        
+        res.json(newFriend);
+        
+    })
+};
+
 
--- app/server.js
+++ app/server.js
@@ -1,21 +1,39 @@
-// Require the needed packages 
-let express = require("express");
-let path = require("path");
-let fs = require("fs");
+// Dependencies
+// =============================================================
+var express = require("express");
+
 
 // Sets up the Express App
 // =============================================================
+// Tells node that we are creating an "express" server
 var app = express();
+// Sets initial port
 var PORT = process.env.PORT || 3000;
-
-// Starts our server 
-app.listen(PORT, function() {
-    console.log("Server is listening on PORT: " + PORT);
-});
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
+
+
 
 
 
