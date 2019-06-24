0680b4700dfd9ead6e8b891ee3c628e474599a95
cmac <chelsey.macneill@gmail.com>
2019-06-23 17:36:59 -0700

    haters gonna hate but it works

--- app/data/friendsData.js
+++ app/data/friendsData.js
@@ -1,6 +1,6 @@
 // Data for filled out surveys
 
-let freindsArray = [
+let friendsArray = [
     {
         "name": "Charles",
         "photo": "https://media.licdn.com/mpr/mpr.jpg",
@@ -15,6 +15,38 @@ let freindsArray = [
             5,
             4,
             1
+        ]
+    },
+    {
+        "name": "JeffB",
+        "photo": "https://media.licdn.com/mpr/mpr.jpg",
+        "scores": [
+            4,
+            1,
+            1,
+            4,
+            2,
+            1,
+            2,
+            5,
+            3,
+            4
+        ]
+    },
+    {
+        "name": "ChelseyM",
+        "photo": "https://media.licdn.com/mpr/mpr.jpg",
+        "scores": [
+            3,
+            2,
+            1,
+            4,
+            4,
+            1,
+            3,
+            1,
+            3,
+            5
         ]
     }
 ];
--- app/server.js
+++ app/server.js
@@ -2,33 +2,30 @@
 // =============================================================
 var express = require("express");
 
-
 // Sets up the Express App
 // =============================================================
 // Tells node that we are creating an "express" server
 var app = express();
+
 // Sets initial port
 var PORT = process.env.PORT || 3000;
 
-// Sets up the Express app to handle data parsing
+// Sets up the Express app to handle data parsing (middleware)
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 
 // ================================================================================
 // ROUTER
 // The below points our server to a series of "route" files.
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
-
-require("./app/routing/apiRoutes")(app);
-require("./app/routing/htmlRoutes")(app);
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
 
 // =============================================================================
 // LISTENER
 // The below code effectively "starts" our server
 // =============================================================================
-
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
 });
