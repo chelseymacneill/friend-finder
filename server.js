fbfc59fc595b6099c5da30f387bfad78dee7e33e
cmac <chelsey.macneill@gmail.com>
2019-06-24 19:00:19 -0700

    Correct packageJSON file

--- server.js
+++ server.js
@@ -19,8 +19,8 @@ app.use(express.json());
 // The below points our server to a series of "route" files.
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
-require("./routing/apiRoutes")(app);
-require("./routing/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 // =============================================================================
 // LISTENER
